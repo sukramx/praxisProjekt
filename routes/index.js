eb0b6013e70bfca2b7949341ae27ba2d0fc036bd
sukramx <sukramx@live.de>
2018-07-29 13:39:31 +0200

    Verwendung der neuen Ordnerstruktur

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-var pathToWatchfolder = 'public/routes/verzeichnis';
-var files = fs.readdirSync('public/routes/verzeichnis/');
+//var pathToWatchfolder = 'public/routes/verzeichnis';
+var pathToWatchfolder = 'public/routes';
+var files = fs.readdirSync('public/routes/');
 var jquery = require('jquery');
 let compareBoxes = ["topleft", "topright", "bottomleft", "bottomright"];
 
@@ -20,10 +21,11 @@ router.get('/', function(req, res, next) {
 try {
     fs.watch(pathToWatchfolder, (eventType, filename) => {
         console.log('event type is: '+eventType);
-        files = fs.readdirSync(pathToWatchfolder);
+        let files = fs.readdirSync(pathToWatchfolder+'/');
+        console.log(files);
         if (filename) {
             console.log('filename provided: '+filename);
-            generateRoutes();
+            generateRoutes(files);
         } else {
             console.log('filename not provided');
         }
@@ -35,7 +37,10 @@ try {
 /**
  * Wird bei Serverstart aufgerufen. Hier werden alle vorhandenen Routen generiert.
  */
-function generateRoutes() {
+function generateRoutes(file) {
+    if (file != undefined){
+        files = file;
+    }
     for (var i = 0; i < files.length; i++) {
         //console.log(files[i]);
         var route = (files[i]);
@@ -49,18 +54,40 @@ function generateRoutes() {
  */
 function generateRoute(route){
     router.get('/' + route, (req, res, next) => {
-        var pictures = fs.readdirSync(pathToWatchfolder+'/'+route+'/samples');
-        //console.log(pictures);
+        //var pictures = fs.readdirSync(pathToWatchfolder+'/'+route+'/samples');
+        var pictures = fs.readdirSync(pathToWatchfolder+'/'+route+"/scenes");
+        console.log(pictures);
+        let picture_json = {
+          "name" : "",
+          "iteration" : 0,
+          "spp" : 0,
+          "total" : 0,
+          "path" : ""
+        };
+
         let pictures_json = {
             "pictures" : []
         };
         for (let i=0; i<pictures.length;i++){
+            const data = require('../'+pathToWatchfolder+'/'+route+'/scenes/'+pictures[i]+'/method-info.json')
+
+            console.log(data);
+
+            let picture = picture_json;
+            picture.name = data.image.name;
+            picture.iteration = data.image.iteration;
+            picture.spp = data.image.spp;
+            picture.total = data.image.total;
+            picture.path = 'routes/'+route+"/scenes/"+data.image.name + "image.png";
+            pictures_json.pictures.push(picture);
 
         }
         res.render('content', { title: route, condition: false, path: route, pictures: pictures, compareBoxes: compareBoxes});
         //console.log(route + " wurde aufgerufen!");
     })
 }
+
+
 
 
 generateRoutes();
